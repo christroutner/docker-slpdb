7d842141a6bcaf159e775ad0c051a898955cc0d2
Chris Troutner <chris.troutner@gmail.com>
2019-10-02 08:36:58 -0700

    Updating run script

--- run-script.sh
+++ run-script.sh
@@ -13,7 +13,16 @@ export db_url=mongodb://172.17.0.1:12301
 # Set the full node IP address and port
 export rpc_host=172.17.0.1
 export rpc_port=8332
+export rpc_user=bitcoin
+export rpc_pass=password
+
 export zmq_incoming_host=172.17.0.1
 export zmq_incoming_port=28332
+
+# Turn off graph search
+export enable_graph_search=0
+
+# Turn off ZMQ output port
+export zmq_outgoing_enable=false
 
 npm start
